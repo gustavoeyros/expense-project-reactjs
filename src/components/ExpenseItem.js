507c5bef6df87a913d3aa418df889b027956cb2c
Gustavo Eyros <gustavoeyros@hotmail.com>
2022-12-27 16:22:09 -0300

    feat: first component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem(){
+    return (
+        <div>
+            <div>10 de Agosto de 2022</div>
+            <div>
+                <h2>Conserto do carro</h2>
+                <div>R$892.50</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
