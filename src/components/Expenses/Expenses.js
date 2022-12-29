e1de8129d11188fb3cff44e3d44a9bcd352d19d3
Gustavo Eyros <gustavoeyros@hotmail.com>
2022-12-28 21:00:29 -0300

    feat: year filter to search expense

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,24 @@
+import { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css'
 import Card from '../UI/Card';
+import ExpensesFilter from '../ExpensesFilter/ExpensesFilter'
 
 const Expenses = (props) =>{
-  
+  const [getYear, setGetYear] = useState('');
+
+  const getYearHandler = (enteredYear) => {
+    setGetYear(parseInt(enteredYear))
+  }
       return (
+
         <Card className='expenses'>
-          {props.info.map((value)=>{
+          <ExpensesFilter onGetYear = {getYearHandler}/>
+          {props.info.filter((value)=>{
+           return value.date.getFullYear() === getYear
+          }).map((value)=>{
           return <ExpenseItem title={value.title} amount={value.amount} date={value.date}/>
-          })}
+          })} 
         </Card>
       );
 }
